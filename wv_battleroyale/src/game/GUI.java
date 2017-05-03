7e13b5f8e77def381df3a0b69e1ec9fd2ff3a3eb
evan <evanthurston@gmail.com>
2017-05-03 10:24:50 -0700

    edit GUI.java

--- wv_battleroyale/src/game/GUI.java
+++ wv_battleroyale/src/game/GUI.java
@@ -1,5 +1,18 @@
 package game;
 
+import java.awt.*;
+
 public class GUI {
-	private int test;
+	public static void createButton(int x, int y, int height, int width, Color color) {
+		
+	}
+	
+	public static void createButton(int x, int y, int height, int width, Color color, String text, int textSize, Color textColor) {
+		createButton(x, y, height, width, color);
+		createText(x, y, text, textSize, textColor);
+	}
+	
+	public static void createText(int x, int y, String text, int textSize, Color textColor) {
+		
+	}
 }
