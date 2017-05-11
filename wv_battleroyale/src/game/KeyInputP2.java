ba19d1bda6c82ee0c156978e437745ae15de4aef
evan <evanthurston@gmail.com>
2017-05-11 09:24:43 -0700

    ijkl xd

--- wv_battleroyale/src/game/KeyInputP2.java
+++ wv_battleroyale/src/game/KeyInputP2.java
@@ -7,7 +7,7 @@ public class KeyInputP2 extends KeyInput
 
 	public KeyInputP2(Fighter player)
 	{
-		super(player, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_UP, KeyEvent.VK_1, KeyEvent.VK_2);
+		super(player, KeyEvent.VK_J, KeyEvent.VK_L, KeyEvent.VK_I, KeyEvent.VK_1, KeyEvent.VK_2);
 	}
 
 }
