4ec6cd0f2b97763d2fc77b41c4edead71e1176b9
evan <evanthurston@gmail.com>
2017-06-01 21:07:32 -0700

    tryna bugfix

--- wv_battleroyale/src/game/GameUtils.java
+++ wv_battleroyale/src/game/GameUtils.java
@@ -87,7 +87,7 @@ public class GameUtils
 	{
 		// Flips the image horizontally
 		AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-		tx.translate(-img.getWidth(null), 0);
+		tx.translate(-img.getWidth(), 0);
 		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
 		img = op.filter(img, null);
 		return img;
