12cd40e298640219e4d3704c9d5e8c7c9b39e4f9
Justin Slavick <justin.slavick@gmail.com>
2017-06-02 00:57:56 -0700

    Text fix

--- wv_battleroyale/src/game/Menus/CassenMenu.java
+++ wv_battleroyale/src/game/Menus/CassenMenu.java
@@ -36,7 +36,7 @@ public class CassenMenu extends Screen
 		GameUtils.self().drawText(width * 5 / 12, 555, "Speed: 7", 30, g);
 		GameUtils.self().drawText(width * 5 / 12, 595, "Health: 500", 30, g);
 		GameUtils.self().drawText(width * 5 / 12, 675, "This character can massively hurt the", 30, g);
-		GameUtils.self().drawText(width * 5 / 12, 675, "opponent if they don't block", 30, g);
+		GameUtils.self().drawText(width * 5 / 12, 715, "opponent if they don't block", 30, g);
 	}
 
 	public Button getBack()
